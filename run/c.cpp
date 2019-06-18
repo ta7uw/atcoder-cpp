60328163f16a5c5aaceb725e6f832fd7bee75a63
Shimbo Takaaki <ta7uwtaka@gmail.com>
2019-06-18 09:56:45 +0900

    Rename Project

---
+++ run/c.cpp
@@ -0,0 +1,36 @@
+//
+// Created by ta7uw on 2019-06-18.
+//
+#include<iostream>
+#include<string>
+#include<cstdio>
+#include<vector>
+#include<cmath>
+#include<algorithm>
+#include<functional>
+#include<iomanip>
+#include<queue>
+#include<ciso646>
+#include<random>
+#include<map>
+#include<set>
+#include<complex>
+#include<bitset>
+#include<stack>
+#include<unordered_map>
+#include<utility>
+#include<cassert>
+
+using namespace std;
+
+const int MOD = 1000000007;
+typedef pair<int, int> P;
+typedef long long ll;
+#define rep(i, m, n) for(int i=m;i<n;i++)
+#define rrep(i, n, m) for(int i=n;i>=m;i--)
+
+
+int main() {
+    cout << "hoge" << endl;
+
+}
