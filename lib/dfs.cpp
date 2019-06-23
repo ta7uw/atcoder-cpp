e4ee28ba441d33787ac95368a7b6dee7a97c11d0
Shimbo Takaaki <ta7uwtaka@gmail.com>
2019-06-24 00:33:53 +0900

    Add dfs

---
+++ lib/dfs.cpp
@@ -0,0 +1,33 @@
+//
+// Created by ta7uw on 2019-06-18.
+//
+#include <iostream>
+#include <vector>
+
+using namespace std;
+using Graph = vector<vector<int>>;
+
+vector<bool> visited;
+
+void dfs(const Graph &G, int v) {
+    seen[v] = true;
+    for (auto next_v : G[v]) {
+        if (seen[next_v]) continue;
+        dfs(G, next_v);
+    }
+}
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+    Graph G(N);
+    for (int i = 0; i < M; ++i) {
+        int a, b;
+        cin >> a >> b;
+        G[a].push_back(b);
+        G[b].push_back(a);
+    }
+
+    visited.assign(N, false);
+    dfs(G, 0);
+}
