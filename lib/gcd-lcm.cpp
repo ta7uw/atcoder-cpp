4cdfad7ebbf172e05a99c354e226c790e3607371
Shimbo Takaaki <ta7uwtaka@gmail.com>
2019-06-24 00:36:25 +0900

    Update gcd-lcm

--- lib/gcd-lcm.cpp
+++ lib/gcd-lcm.cpp
@@ -2,11 +2,9 @@
 // Created by ta7uw on 2019-06-24.
 //
 ll gcd(ll a, ll b) {
-    if (b == 0) {
-        return a;
-    } else {
-        return gcd(b, a % b);
-    }
+    if (x < y)swap(x, y);
+    if (y == 0)return x;
+    return gcd(y, x % y);
 }
 
 ll lcm(ll a, ll b) {
