6c9bf934becc3802ca99004ae7e45d8afee43669
Shimbo Takaaki <ta7uwtaka@gmail.com>
2019-07-02 00:13:21 +0900

    Add UnionFind

---
+++ lib/union_find.cpp
@@ -0,0 +1,51 @@
+//
+// Created by ta7uw on 2019-07-02.
+//
+
+struct UnionFind {
+    vector<int> par;
+    vector<int> rank;
+
+    UnionFind(int n = 1) {
+        init(n);
+    }
+
+    void init(n = 1) {
+        par.resize(n + 1);
+        rank.resize(n + 1);
+        for (int i = 0; i <= n; ++i) {
+            par[i] = i;
+            rank[i] = 0;
+        }
+    }
+
+    int find(int x) {
+        if (par[x] == x) {
+            return x;
+        } else {
+            int r = find(par[x]);
+            return par[x] = r;
+        }
+    }
+
+    bool is_same(int x, int y) {
+        return find(x) == find(y);
+    }
+
+    bool merge(int x, int y) {
+        x = find(x);
+        y = find(y);
+        if (x == y) {
+            return false;
+        } else {
+            if (rank[x] < rank[y]) {
+                swap(x, y);
+            }
+            if (rank[x] == ranl[y]) {
+                ++rank[x];
+            }
+            par[y] = x;
+            return true
+        }
+    }
+};
