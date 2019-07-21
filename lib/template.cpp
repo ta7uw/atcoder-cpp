b593e686cb0056c6c1b03a23eff1492d2ac57783
Shimbo Takaaki <ta7uwtaka@gmail.com>
2019-07-21 12:57:04 +0900

    Fix template

--- lib/template.cpp
+++ lib/template.cpp
@@ -4,15 +4,17 @@ using namespace std;
 
 typedef pair<int, int> P;
 typedef long long ll;
-#define rep(i, n) for(int i=0;i<n;i++)
-#define rep2(i, m, n) for(int i=m;i<n;i++)
-#define rrep(i, n, m) for(int i=n;i>=m;i--)
+#define rep(i, n) for(int i=0;i<(n);i++)
+#define rep2(i, m, n) for(int i=m;i<(n);i++)
+#define rrep(i, n, m) for(int i=n;i>=(m);i--)
 using Graph = vector<vector<int>>;
 const int dx[4] = {1, 0, -1, 0};
 const int dy[4] = {0, 1, 0, -1};
 const ll MOD = 1000000007;
 #ifdef __DEBUG
+
 #include "cpp-pyprint/pyprint.h"
+
 #endif
 
 void Main() {
