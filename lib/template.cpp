e4d00a600b5b5e75cbfd2d8530c200e8d83b6ffd
Shimbo Takaaki <ta7uwtaka@gmail.com>
2019-08-31 13:59:14 +0900

    Add inf to template

--- lib/template.cpp
+++ lib/template.cpp
@@ -11,6 +11,8 @@ using Graph = vector<vector<int>>;
 const int dx[4] = {1, 0, -1, 0};
 const int dy[4] = {0, 1, 0, -1};
 const ll MOD = 1000000007;
+const int INF = 1000000000;
+const ll LINF = 1000000000000000000L;
 #ifdef __DEBUG
 
 #include "cpp-pyprint/pyprint.h"
