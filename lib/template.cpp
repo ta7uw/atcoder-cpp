39bd4a17d974cd98530375ac2370ffe1e45e98ab
Shimbo Takaaki <ta7uwtaka@gmail.com>
2019-08-31 14:03:06 +0900

    Add ddx and ddy to template

--- lib/template.cpp
+++ lib/template.cpp
@@ -10,6 +10,8 @@ typedef long long ll;
 using Graph = vector<vector<int>>;
 const int dx[4] = {1, 0, -1, 0};
 const int dy[4] = {0, 1, 0, -1};
+const int ddx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int ddy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
 const ll MOD = 1000000007;
 const int INF = 1000000000;
 const ll LINF = 1000000000000000000L;
