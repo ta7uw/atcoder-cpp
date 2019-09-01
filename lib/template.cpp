f28485bdbdad9f0490d853757d5c83a85fb0d0f9
Shimbo Takaaki <ta7uwtaka@gmail.com>
2019-09-01 12:40:31 +0900

    Update template

--- lib/template.cpp
+++ lib/template.cpp
@@ -5,17 +5,16 @@ using namespace std;
 using ll = long long;
 using P = pair<ll, ll>;
 using edge = pair<ll, P>;
-#define rep(i, n) for(ll i=0;i<(n);i++)
-#define rep2(i, m, n) for(ll i=m;i<(n);i++)
-#define rrep(i, n, m) for(ll i=n;i>=(m);i--)
+#define rep(i, n) for(ll i=0;(ll)i<(n);i++)
+#define rep2(i, m, n) for(ll i=m;(ll)i<(n);i++)
+#define rrep(i, n, m) for(ll i=n;(ll)i>=(m);i--)
 using Graph = vector<vector<ll>>;
 const int dx[4] = {1, 0, -1, 0};
 const int dy[4] = {0, 1, 0, -1};
 const int ddx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
 const int ddy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
 const ll MOD = 1000000007;
-const int INF = 1000000000;
-const ll LINF = 1000000000000000000L;
+const ll INF = 1000000000000000000L;
 #ifdef __DEBUG
 
 #include "cpp-pyprint/pyprint.h"
