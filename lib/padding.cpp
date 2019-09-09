dcea5d25ab5fa133452ede6b667cb52a96840fbc
Shimbo Takaaki <ta7uwtaka@gmail.com>
2019-09-10 01:31:42 +0900

    Add padding to template

---
+++ lib/padding.cpp
@@ -0,0 +1,16 @@
+using namespace std;
+
+// 右詰め
+string padding(string s, int width, char fill) {
+    ostringstream oss;
+    oss << setw(width) << setfill(fill) << s;
+    return oss.str();
+}
+
+
+// 左詰め
+string padding_left(string s, int width, char fill) {
+    ostringstream oss;
+    oss << left << setw(width) << setfill(fill) << s;
+    return oss.str();
+}
