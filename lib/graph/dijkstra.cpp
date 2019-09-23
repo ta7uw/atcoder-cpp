bd13d26bd853591558efbdafb005061cdd3b4aeb
Shimbo Takaaki <ta7uwtaka@gmail.com>
2019-09-23 20:40:52 +0900

    Add dijkstra

---
+++ lib/graph/dijkstra.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using P = pair<ll, ll>;
+using Graph = vector<vector<ll>>;
+#define rep(i, n) for(ll i=0;i<(ll)(n);i++)
+#define rep2(i, m, n) for(ll i=m;i<(ll)(n);i++)
+#define rrep(i, n, m) for(ll i=n;i>=(ll)(m);i--)
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+const int ddx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int ddy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+const ll MOD = 1000000007;
+const ll INF = 1000000000000000000L;
+
+struct edge {
+    int from, to;
+    ll cost;
+};
+
+class Dijkstra {
+    int V;
+    vector<vector<edge>> edges;
+    vector<ll> dist;
+    vector<ll> prev;
+
+public:
+    Dijkstra(int v) : V(v) {
+        edges.resize(V);
+        dist.resize(V, INF);
+        prev.resize(V, INF);
+    }
+
+    void add_edge(int from, int to, ll cost) {
+        edges[from].push_back({from, to, cost});
+    }
+
+    /**
+     * @param goal 0-indexed
+     * @return return cost to reach the goal. return INF if the goal is unreachable.
+     */
+    ll get_cost(int goal) {
+        return dist[goal];
+    }
+
+    void exec(int start) {
+        dist[start] = 0;
+        priority_queue<P, vector<P>, greater<>> queue;
+        queue.push({dist[start], start});
+        while (!queue.empty()) {
+            P p = queue.top();
+            queue.pop();
+            ll prev_cost = p.first;
+            ll src = p.second;
+            if (dist[src] < prev_cost) continue;
+
+            for (edge &e : edges[src]) {
+                ll to = e.to;
+                ll cost = e.cost;
+                if (cost != INF && dist[to] > dist[src] + cost) {
+                    dist[to] = dist[src] + cost;
+                    queue.push({dist[to], to});
+                    prev[to] = src;
+                }
+            }
+        }
+    }
+};
+
+/**
+ * ABC 035 D - トレジャーハント
+ * https://atcoder.jp/contests/abc035/submissions/7668866
+ */
+void Main() {
+    int N, M, T;
+    cin >> N >> M >> T;
+    vector<ll> t_list(N);
+    rep(i, N) cin >> t_list[i];
+    Dijkstra dijkstra1(N);
+    Dijkstra dijkstra2(N);
+    rep(i, M) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        a--;
+        b--;
+        dijkstra1.add_edge(a, b, c);
+        dijkstra2.add_edge(b, a, c);
+    }
+
+    dijkstra1.exec(0);
+    dijkstra2.exec(0);
+    ll ans = t_list[0] * T;
+    rep2(i, 1, N) {
+        ll go = dijkstra1.get_cost(i);
+        ll back = dijkstra2.get_cost(i);
+        if (go != INF && back != INF) ans = max(ans, t_list[i] * (T - go - back));
+    }
+    cout << ans << '\n';
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    Main();
+}
