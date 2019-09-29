9557d15942c1b8488c97c9b09136f546199f2ca8
Shimbo Takaaki <ta7uwtaka@gmail.com>
2019-09-29 18:09:32 +0900

    Fix divisor

--- lib/number/divisor.cpp
+++ lib/number/divisor.cpp
@@ -1,7 +1,7 @@
 // 約数列挙
 vector<ll> divisor(ll N) {
     vector<ll> res;
-    for (ll i = 1L; i * i< N; ++i) {
+    for (ll i = 1L; i * i <= N; ++i) {
         if (N % i == 0) {
             res.push_back(i);
             ll j = N / i;
