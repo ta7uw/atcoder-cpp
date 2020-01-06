a8391b7fd1e961aee2c4fc942885a9a5863e8e20
Shimbo Takaaki <ta7uwtaka@gmail.com>
2020-01-07 00:15:54 +0900

    Add mod_inverse usage

--- lib/number/mod_inverse.cpp
+++ lib/number/mod_inverse.cpp
@@ -12,18 +12,13 @@ const int dx[4] = {1, 0, -1, 0};
 const int dy[4] = {0, 1, 0, -1};
 const int ddx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
 const int ddy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
-const ll MOD = 998244353;
+const ll MOD = 1000000007;
 const ll INF = 1000000000000000000L;
-#ifdef __DEBUG
 
 /**
- * For DEBUG
- * https://github.com/ta7uw/cpp-pyprint
+ * Library
+ * --------------------------------------------------------
  */
-#include "cpp-pyprint/pyprint.h"
-
-#endif
-
 ll mod_inverse(ll a, ll mod) {
     ll b = mod, u = 1, v = 0;
     while (b) {
@@ -37,9 +32,39 @@ ll mod_inverse(ll a, ll mod) {
     if (u < 0) u += mod;
     return u;
 }
+/**
+ * --------------------------------------------------------
+ */
 
+/**
+ * D. Santa's Bot
+ * https://codeforces.com/contest/1279/problem/D
+ */
 void Main() {
-
+    ll N;
+    cin >> N;
+    vector<vector<int>> A(N);
+    map<ll, ll> cnt;
+    rep(i, N) {
+        int K;
+        cin >> K;
+        vector<int> tmp(K, 0);
+        rep(k, K) cin >> tmp[k];
+        A[i] = tmp;
+        rep(k, K) cnt[A[i][k]]++;
+    }
+    ll ans = 0;
+    ll mod = mod = 998244353;
+    ll rev = mod_inverse(N * N, mod);
+    rep(i, N) {
+        ll k = A[i].size();
+        ll k_inv = mod_inverse(k, mod);
+        for (ll a: A[i]) {
+            ans += (((k_inv * rev) % mod) * cnt[a]) % mod;
+            ans %= mod;
+        }
+    }
+    cout << ans << '\n';
 }
 
 int main() {
