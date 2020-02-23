6c2e784e879e6f732a8431ffb7d48e8c5aa75496
Shimbo Takaaki <ta7uwtaka@gmail.com>
2020-02-23 10:29:10 +0900

    Refactor combination library

--- lib/number/combination.cpp
+++ lib/number/combination.cpp
@@ -1,32 +1,67 @@
 const int MAX = 510000;
-const int MOD = 1000000007;
-
 long long fac[MAX], finv[MAX], inv[MAX];
+bool initialized = false;
 
-// テーブルを作る前処理
-void COMinit() {
+/**
+ * @fn
+ * This function must be called at first.
+ */
+void combination_init() {
+    initialized = true;
     fac[0] = fac[1] = 1;
     finv[0] = finv[1] = 1;
     inv[1] = 1;
-    for (int i = 2; i < MAX; i++){
+    for (int i = 2; i < MAX; i++) {
         fac[i] = fac[i - 1] * i % MOD;
-        inv[i] = MOD - inv[MOD%i] * (MOD / i) % MOD;
+        inv[i] = MOD - inv[MOD % i] * (MOD / i) % MOD;
         finv[i] = finv[i - 1] * inv[i] % MOD;
     }
 }
 
-// 二項係数計算
-long long COM(int n, int k){
+/**
+ * @fn
+ * Calculates the combination  ( nCK % p: N choose K mod p).
+ *
+ * @brief constraint: 1 ≤ k ≤ n ≤ 10^7
+ * @return nCK % MOD
+ */
+long long combination(int n, int k) {
+    assert(initialized);
     if (n < k) return 0;
     if (n < 0 || k < 0) return 0;
     return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
 }
 
-int main() {
-    // 前処理
-    COMinit();
+/**
+ * @fn
+ * Calculates the factorial of the number.
+ *
+ * @return N! % MOD
+ */
+long long factorial(int n) {
+    assert(initialized);
+    if (n < 0) return 0;
+    return fac[n] % MOD;
+}
 
-    // 計算例
-    cout << COM(100000, 50000) << endl;
-    return 0;
+/**
+ * @fn
+ * Calculates the combination ( nCK % p: N choose K mod p).
+ *
+ * @brief constraint: 1 ≤ k ≤ 10^7 and 1 ≤ n ≤ 10^9
+ * @return nCK % MOD
+ */
+long long combination2(int n, int k) {
+    assert(initialized);
+    if (n < k) return 0;
+    if (n < 0 || k < 0) return 0;
+    ll res = 1;
+    k = min(k, n - k);
+    for (long long x = 1; x <= k; x++) {
+        res *= (n - x + 1);
+        res %= MOD;
+        res *= inv[x];
+        res %= MOD;
+    }
+    return res;
 }
