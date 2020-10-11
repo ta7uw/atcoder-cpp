2a79ac5abeef271edfe1c7580432e144cba1b0a2
Shimbo Takaaki <ta7uwtaka@gmail.com>
2020-10-11 12:01:09 +0900

    Update template

--- template.cpp
+++ template.cpp
@@ -1,9 +1,9 @@
 #include <bits/stdc++.h>
-// #include <atcoder/all>
+#include <atcoder/all>
 
 using namespace std;
-// using namespace atcoder;
-
+using namespace atcoder;
+// @formatter:off
 using ll = long long;
 using P = pair<ll, ll>;
 using Graph = vector<vector<ll>>;
@@ -16,18 +16,22 @@ const int ddx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
 const int ddy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
 const ll MOD = 1000000007;
 const ll INF = 1000000000000000000L;
+template<typename T> bool chmax(T &a, const T &b) {if (a < b){a = b;return true;}else{return false;}}
+template<typename T> bool chmin(T &a, const T &b) {if (a > b){a = b;return true;}else{return false;}}
+
 #ifdef __DEBUG
 /**
  * For DEBUG
  * https://github.com/ta7uw/cpp-pyprint
  */
 #include "cpp-pyprint/pyprint.h"
+
 #else
 #define print(...)
 #endif
+// @formatter:on
 
 void solve() {
-
 
 }
 
